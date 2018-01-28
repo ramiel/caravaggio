d24e9485b05625ae563d607d349468554788e771
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2018-01-28 13:20:18 +0100

    Re-adapt parser

--- test/libs/optionsParser.test.js
+++ test/libs/optionsParser.test.js
@@ -1,9 +1,9 @@
-const optionsParser = require('../../src/libs/optionsParser');
+const { createCallStack } = require('../../src/libs/parser');
 
 describe('option parser', () => {
   test('parse rotate option', () => {
     const options = 'rotate_90';
-    const parsed = optionsParser(options);
+    const parsed = createCallStack(options);
     expect(parsed).toEqual([
       ['rotate', 90],
     ]);
