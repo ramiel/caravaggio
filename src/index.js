67cbfe14e24a0eb19ffcae4d938f49bdf12c2d68
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2018-01-28 12:55:21 +0100

    Little example of option parsing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const { send } = require('micro')
 const { router, get } = require('microrouter')
+const optionsParser = require('./libs/optionsParser');
 
-const index = async (req, res) =>
-  send(res, 200, await Promise.resolve(`Hello ${req.params.image}`))
+const index = async (req, res) => {
+  send(res, 200, await Promise.resolve(optionsParser(req.params.options)));
+}
 
 module.exports = router(
   get('/:options/:image', index)
