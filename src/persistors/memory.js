9c857047dd761ae4aa2ce4f1407e83176117122b
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2018-01-29 20:42:50 +0100

    Fix normalizer
    Do not touch property others than operations

--- src/persistors/memory.js
+++ src/persistors/memory.js
@@ -3,12 +3,16 @@ const persisted = {};
 module.exports = () => ({
   exists: filename => Promise.resolve(!!persisted[filename]),
 
-  read: filename => Promise.resolve(persisted[filename]
-    ? {
-      type: 'buffer',
-      buffer: persisted[filename],
-    }
-    : null),
+  read: (filename) => {
+    console.log(`checking file ${filename}`);
+    return Promise.resolve(persisted[filename]
+      ? {
+        type: 'buffer',
+        buffer: persisted[filename],
+      }
+      : null);
+  },
+
 
   save: (filename, buffer) => {
     persisted[filename] = buffer;
