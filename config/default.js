ab7cbf6c56445a096c27d37eecbff2428bb59187
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2018-01-29 19:40:21 +0100

    Allow a diskless persistor

--- config/default.js
+++ config/default.js
@@ -3,10 +3,32 @@ const path = require('path');
 module.exports = {
   port: parseInt(process.env.PORT, 10) || 8565,
   persistor: {
-    type: 'file', // availabe: file, memory, s3
+    /**
+     * Available types of persistors:
+     *
+     * file:     Save images on local disk. On by default is the easiest to configure
+     * memory:   Save images in memory. Useful on development, can drain a lot of memory. Do not use
+     *           on production
+     * diskless: Never save the images. The images will be re-calculated each time. Very useful if
+     *           you plan to put a cache on front of this service, like CloudFront or CloudFlare.
+     * s3:       Save the images on Amazon S3
+     */
+    type: 'file',
     options: {
       basePath: path.resolve(__dirname, '../cache'),
     },
+
+    /**
+     * Memory
+     * type: 'memory',
+     * options: {}
+     */
+
+    /**
+     * Disk-less
+     * type: 'diskless',
+     * options: {}
+     */
 
     /**
      * S3 persistor
