052e356289f197fd5f7f1617b54aca5b2f8675c0
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2018-01-30 20:38:05 +0100

    Rename diskless persistor to none

--- src/index.js
+++ src/index.js
@@ -1,25 +1,6 @@
-const config = require('config');
 const { router, get } = require('microrouter');
 const indexRoute = require('./routes/index');
 const Cache = require('./cache');
 
-const persistorType = config.get('persistor.type');
-const persistorOptions = config.get('persistor.options');
 
-let persistor;
-switch (persistorType) {
-  case 'file':
-    // eslint-disable-next-line global-require
-    persistor = require('./persistors/file')(persistorOptions);
-    break;
-
-  case 'memory':
-    // eslint-disable-next-line global-require
-    persistor = require('./persistors/memory')(persistorOptions);
-    break;
-
-  default:
-    throw new Error(`Invalid persistor ${persistorType}. Check your configuration`);
-}
-
-module.exports = router(get('/*/*', indexRoute(Cache(persistor))));
+module.exports = persistor => router(get('/*/*', indexRoute(Cache(persistor))));
