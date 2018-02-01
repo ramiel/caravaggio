55d3f19c69e23f7235c28558c1b147497cb32b03
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2018-02-02 00:39:35 +0100

    Program can be run in development mode

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
 const config = require('config');
 const micro = require('micro');
-const path = require('path');
-const router = require('./src');
+const app = require('./src');
 
-const persistorType = config.get('persistor.type');
-const persistorOptions = config.get('persistor.options');
 const port = config.get('port');
 
-let persistor;
-
-try {
-  // eslint-disable-next-line global-require, import/no-dynamic-require
-  persistor = require(path.join(__dirname, 'src/persistors', persistorType))(persistorOptions);
-} catch (e) {
-  throw new Error(`Invalid persistor ${persistorType}. Check your configuration: ${e.message}`);
-}
-
-const server = micro(router(persistor));
+const server = micro(app);
 server.listen(port);
 
 console.log(`Server started.
-Listen on port: ${port}.
-Persistor: ${persistorType}
-Persistor Options: ${Object.entries(persistorOptions).map(([key, value]) => `
-  ${key}: ${value}`).join('')}
-`);
+Listen on port: ${port}.`);
+// Persistor: ${persistorType}
+// Persistor Options: ${Object.entries(persistorOptions).map(([key, value]) => `
+//   ${key}: ${value}`).join('')}
 
